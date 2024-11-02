ed6fa1b6d98e3a24a647edeeb9c1cb9beac17c8f
sanja-stanciulescu <anacristina.stanciulescu@gmail.com>
2024-11-02 23:31:29 +0200

    Implemented action: getPlayerDeck()

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,17 +6,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import checker.CheckerConstants;
-import fileio.Input;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import fileio.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
- * The entry point to this homework. It runs the checker that tests your implentation.
+ * The entry point to this homework. It runs the checker that tests your implementation.
  */
 public final class Main {
     /**
@@ -74,19 +76,50 @@ public final class Main {
          *
          * How to add output to the output array?
          * There are multiple ways to do this, here is one example:
-         *
-         * ObjectMapper mapper = new ObjectMapper();
-         *
-         * ObjectNode objectNode = mapper.createObjectNode();
-         * objectNode.put("field_name", "field_value");
-         *
-         * ArrayNode arrayNode = mapper.createArrayNode();
-         * arrayNode.add(objectNode);
-         *
-         * output.add(arrayNode);
-         * output.add(objectNode);
-         *
          */
+        //System.out.println(inputData);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ArrayNode objectNode = mapper.createArrayNode();
+
+        ArrayList<GameInput> games = inputData.getGames();
+        for (GameInput game : games) {
+            StartGameInput startGame = game.getStartGame();
+            ArrayList<ActionsInput> actions = game.getActions();
+            for (ActionsInput action : actions) {
+                switch (action.getCommand()) {
+                    case "getPlayerDeck":
+                        ObjectNode getPlayerDeckNode = mapper.createObjectNode();
+                        getPlayerDeckNode.put("command", action.getCommand());
+                        getPlayerDeckNode.put("playerIdx", action.getPlayerIdx());
+                        ArrayList<CardInput> deck;
+                        if (action.getPlayerIdx() == 1) {
+                            deck = inputData.getPlayerOneDecks().getDecks().get(startGame.getPlayerOneDeckIdx());
+                        } else {
+                            deck = inputData.getPlayerTwoDecks().getDecks().get(startGame.getPlayerTwoDeckIdx());
+                        }
+                        ArrayNode deckArray = mapper.createArrayNode();
+                        for (CardInput card : deck) {
+                            ObjectNode cardNode = mapper.convertValue(card, ObjectNode.class);
+                            deckArray.add(cardNode);
+                        }
+                        getPlayerDeckNode.set("output", deckArray);
+                        output.add(getPlayerDeckNode);
+                        break;
+                    default:
+                        System.out.println("Unknown command: " + action.getCommand());
+                        break;
+                }
+            }
+        };
+        //objectNode.put("command", inputData.getPlayerOneDecks().getNrDecks());
+        //objectNode.put("playerIdx", 2);
+
+        //ArrayNode arrayNode = mapper.createArrayNode();
+        //arrayNode.add(objectNode);
+
+        //output.add(objectNode);
 
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
         objectWriter.writeValue(new File(filePath2), output);
